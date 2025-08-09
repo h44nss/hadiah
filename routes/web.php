15f190c87d848897c7f0d9d9cb8f59a39b4d67db
Burhan <133931161+h44nss@users.noreply.github.com>
2025-08-10 01:53:55 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::middleware('auth')->group(function () {
 });
 
 
-Route::prefix('admin')->name('admin.')->group(function () {
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
     // Events
